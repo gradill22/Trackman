00500d09d9ee4621cf33659a35b338db7072ed10
Grady Dillon <gradydillon22@gmail.com>
2023-10-12 20:29:03 -0400

    Simplified script using Seaborn

--- pitchers.py
+++ pitchers.py
@@ -62,27 +62,33 @@ def plot_field(fence_color='black', base_color='black'):
     base_x = [0, -90 / sqrt(2), 0, 90 / sqrt(2), 0]
     base_y = [0, 90 / sqrt(2), 90 * sqrt(2), 90 / sqrt(2), 0]
 
-    plt.plot(fence_x, fence_y, color=fence_color)
-    plt.plot(base_x, base_y, color=base_color, marker='s')
+    plt.plot(fence_x, fence_y, c=fence_color)
+    plt.plot(base_x, base_y, c=base_color, marker='s')
 
 
 def plot_strike_zone(color='black'):
     x_marks = [-0.71, -0.24, 0.24, 0.71]
     y_marks = [1.65, 2.32, 2.99, 3.65]
 
-    for x in x_marks:
+    for x, y in zip(x_marks, y_marks):
         plt.plot([x, x], [y_marks[0], y_marks[-1]], c=color)
-    for y in y_marks:
         plt.plot([x_marks[0], x_marks[-1]], [y, y], c=color)
+
+
+def pitches_and_hue(data: pd.DataFrame) -> (pd.DataFrame, str):
+    hue = 'TaggedPitchType'
+    pitches = data[data[hue] != 'Undefined']
+    if len(pitches) == 0 and 'AutoPitchType' in data.columns:
+        hue = 'AutoPitchType'
+        pitches = data[data[hue] != 'Undefined']
+    return pitches, hue
 
 
 # Building the Horizontal vs Vertical Break of each pitch scatter plot
 def horz_vert_break_chart(name: str, data: pd.DataFrame, file_name: str) -> None:
-    hue = 'TaggedPitchType'
-    pitches = data[data[hue] != 'Undefined']
+    pitches, hue = pitches_and_hue(data)
     if len(pitches) == 0:
-        hue = 'AutoPitchType'
-        pitches = data[data[hue] != 'Undefined']
+        return
 
     sns.scatterplot(pitches, x='HorzBreak', y='InducedVertBreak', hue=hue)
     plt.axvline(x=0, color='black', linestyle=':')
@@ -106,36 +112,31 @@ def pitch_calls_chart(name: str, data: pd.DataFrame, file_name: str) -> None:
 
 
 def pitch_summaries(name: str, data: pd.DataFrame, file_name: str) -> None:
-    hue = 'TaggedPitchType'
-    pitches = data[data[hue] != 'Undefined']
+    pitches, hue = pitches_and_hue(data)
     if len(pitches) == 0:
-        hue = 'AutoPitchType'
-        pitches = data[data[hue] != 'Undefined']
+        return
 
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 7))
-
-    index = 0
-    for axs in axes:
-        for ax in axs:
-            dp = data_points[index]
-            ax = sns.violinplot(data=pitches, y=dp, hue=hue, ax=ax)
-            ax.set_ylabel(units[dp])
-            ax.set_title(translation[dp])
-            ax.legend()
-            ax.set_xticklabels([])
-            index += 1
+    for i in range(len(data_points)):
+        dp = data_points[i]
+        ax = axes[i // 2, i % 2]
+        sns.violinplot(data=pitches, y=dp, hue=hue, ax=ax)
+        ax.set_ylabel(units[dp])
+        ax.set_title(translation[dp])
+        ax.legend()
+        ax.set_xticklabels([])
 
     fig.suptitle(f'Pitch Arsenal Summary - {name}')
     plt.subplots_adjust(wspace=0.2, hspace=0.2)
-    plt.savefig(f'{file_name}_arsenal_summary.png', dpi=DPI)
+    plt.savefig(fname=f'{file_name}_arsenal_summary.png', dpi=DPI)
 
 
 def spray_chart(name: str, data: pd.DataFrame, file_name: str) -> None:
     results = data[data['PlayResult'] != 'Undefined']
+    results = results.dropna(axis=0, subset=['Distance', 'Direction'])
     if len(results) == 0:
         return
 
-    results = results.dropna(axis=0)
     results['Distance*sin(Direction)'] = results['Distance'] * np.sin(np.radians(results['Direction']))
     results['Distance*cos(Direction)'] = results['Distance'] * np.cos(np.radians(results['Direction']))
 
@@ -145,22 +146,26 @@ def spray_chart(name: str, data: pd.DataFrame, file_name: str) -> None:
     plt.ylabel('Hit Location (ft)')
     plt.title(f'Spray Chart - {name}')
     plt.legend()
-    plt.savefig(f'{file_name}_spray_chart.png', dpi=DPI)
+    plt.savefig(fname=f'{file_name}_spray_chart.png', dpi=DPI)
 
 
 def create_charts(name: str, date: str, pitcher_data: pd.DataFrame, save_file: str) -> None:
+    date = date.replace('-', '_')
+    name = name.replace(', ', '_')
+
     # Create the new save folder to place all .png images in
-    path = os.path.join(save_file, name.replace(', ', '_'), date)
+    path = os.path.join(save_file, name, date)
     if pitcher_data['PitcherTeam'].values[0] != HOME_TEAM:
-        path = os.path.join(save_file, '_OPPONENTS', date, name.replace(', ', '_'))
+        path = os.path.join(save_file, '_OPPONENTS', date, name)
 
     try:
         os.makedirs(path)
     except FileExistsError:
         pass
 
-    file_name = f"{name.replace(', ', '_')}_{date}"
+    file_name = '_'.join((name, date))
     full_path = os.path.join(path, file_name)
+    name = ' '.join(name.split('_')[::-1])
 
     pitch_summaries(name, pitcher_data, full_path)
     plt.close()
@@ -179,7 +184,7 @@ def main():
     for name_and_date, pitcher_data in tqdm(pitchers, desc='Generating visualizations'):
         name, date = name_and_date
         name = str(name).strip()
-        date = str(date).replace('-', '_').strip()
+        date = str(date).strip()
         create_charts(name, date, pitcher_data, save_file)
 
     os.startfile(save_file)
